19893b0d9134130ef659ecd18117d1f6406fba36
guverz <guverz1994@mail.ru>
2024-11-24 19:53:54 +0800

    added edit, toggle funcs and implemented table

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 	todos.add("Cook the chicken")
 	todos.add("Mop the floor")
 	todos.add("Clean the dishes")
-	//
-	fmt.Printf("%+v\n\n", todos)
+
 	todos.del(1)
-	fmt.Printf("%+v\n\n", todos)
+	todos.toggle(1)
+	todos.edit(0, "Cook the turkey")
+	todos.print()
 }
--- todo.go
+++ todo.go
@@ -3,7 +3,11 @@ package main
 import (
 	"errors"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
+
+	"github.com/aquasecurity/table"
 )
 
 type Todo struct {
@@ -26,13 +30,19 @@ func (todos *Todos) add(title string) {
 	*todos = append(*todos, todo)
 }
 
-func (todos *Todos) edit(command string) {
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
 
+	t[index].Title = title
+	return nil
 }
 
 func (todos *Todos) validateIndex(index int) error {
 	if index < 0 || index >= len(*todos) {
-		err := errors.New("Index is not present")
+		err := errors.New("index is not present")
 		fmt.Println(err)
 		return err
 	} else {
@@ -52,4 +62,39 @@ func (todos *Todos) del(index int) error {
 
 func (todos *Todos) list() string {
 	return "foobar"
+}
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+	isCompleted := t[index].Completed
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+
+	}
+
+	t[index].Completed = !isCompleted
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "Created at", "Completed at")
+	for index, t := range *todos {
+		completed := "False"
+		completedAt := ""
+		if t.Completed {
+			completed = "True"
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC1123)
+			}
+		}
+		table.AddRow(strconv.Itoa(index), t.Title, completed, t.CreatedAt.Format(time.RFC1123), completedAt)
+	}
+	table.Render()
 }
